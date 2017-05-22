2b153ad4914222fba57ecab80c278328b78d1a75
paulokatho <pkatho@gmail.com>
2017-05-21 23:23:41 -0300

    Criando página 500 de erro ao acessar o servidor

--- src/main/java/com/algaworks/brewer/controller/CervejasController.java
+++ src/main/java/com/algaworks/brewer/controller/CervejasController.java
@@ -50,6 +50,7 @@ public class CervejasController {
 	public ModelAndView cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes) {
 		if (result.hasErrors()) {
 			return novo(cerveja);
+			//throw new RuntimeException(); teste para página 500
 		}
 		
 		cadastroCervejaService.salvar(cerveja);
